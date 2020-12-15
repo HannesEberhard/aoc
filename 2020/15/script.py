f151b07c1f2c46d369803002c81cdd5296a14033
Hannes Eberhard <hannes.eberhard@hotmail.com>
2020-12-15 09:23:57 +0100

    15 2020

--- 2020/15/script.py
+++ 2020/15/script.py
@@ -1,17 +1,37 @@
 
 from common import Day
+from collections import defaultdict
 
 
 class Day_2020_15(Day):
 
     def parse(self):
-        return super().parse()
+        return [int(x) for x in self.input.split(",")]
+
+    def simulate(self, initialization, n):
+        history = dict()
+        for turn in range(1, len(initialization) + 1):
+            number = initialization[turn - 1]
+            history[number] = [turn]
+        for turn in range(1 + len(initialization), n + 1):
+            if len(history[number]) != 2:
+                number = 0
+            else:
+                number = history[number][1] - history[number][0]
+            if number in history:
+                history[number].append(turn)
+            else:
+                history[number] = [turn]
+            if len(history[number]) == 3:
+                history[number] = history[number][1:3]
+        return number
+
 
     def part_1(self):
-        return None
+        return self.simulate(self.parsed, 2020)
 
     def part_2(self):
-        return None
+        return self.simulate(self.parsed, 30000000)
 
 
 if __name__ == "__main__":
